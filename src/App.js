4d3cb74c15df5177dc835e8a30f2a4b562423923
Tharun-coder <tharunece95@gmail.com>
2021-11-14 12:22:28 +0530

    chore: minor updates

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { HashRouter as Router, Switch, Route } from "react-router-dom";
 import { makeStyles } from "@material-ui/core";
 import Header from "./components/Header";
 import HomeScreen from "./screens/HomeScreen";
